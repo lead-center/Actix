41235bcc6ebda338c6543db72cf177a9eaffb335
LEAD Center <lead@ocf.berkeley.edu>
2023-04-04 08:53:45 -0700

    pre ex

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,31 @@
 use actix_files::Files;
 use actix_web::{App, HttpResponse, HttpServer, Result, get};
+#[cfg(unix)]
 use actix_web::rt::net::UnixListener;
 
 #[get("/")]
 async fn index() -> Result<HttpResponse> {
     Ok(HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
-        .body(include_str!("../wasm/dist/index.html")))
+        .body(include_str!("../../wasm/dist/index.html")))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let mut server = HttpServer::new(|| {
         App::new()
-            .service(Files::new("/", "../wasm/dist/index.html").index_file("index.html"))
+            .service(Files::new("/", "../../wasm/dist/index.html").index_file("index.html"))
     });
-
-    
 
     let socket = std::env::var("SOCK");
 
-    let server = if let Ok(socket) = socket {
+    if let Ok(socket) = socket {
+        #[cfg(unix)]
         let listener = UnixListener::bind(socket).unwrap();
-        listener
+        server = server.bind_uds(socket)?;
+        return server.run().await
     } else {
         server = server.bind(("127.0.0.1", 8080))?;
-        server
+        return server.run().await
     }
-
-    server.run().await
 }
