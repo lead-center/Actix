3474332129ee0f3bd567344f5dabd0c3abdf9cb4
LEAD Center <lead@ocf.berkeley.edu>
2023-04-04 09:16:29 -0700

    it works!

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,32 @@
-use actix_files::Files;
-use actix_web::{App, HttpResponse, HttpServer, Result, get};
+use actix_web::{get, web, HttpRequest};
 #[cfg(unix)]
-use actix_web::rt::net::UnixListener;
+use actix_web::{middleware, App, Error, HttpResponse, HttpServer};
+use actix_files::Files;
 
-#[get("/")]
-async fn index() -> Result<HttpResponse> {
-    Ok(HttpResponse::Ok()
-        .content_type("text/html; charset=utf-8")
-        .body(include_str!("../../wasm/dist/index.html")))
-}
-
+#[cfg(unix)]
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let mut server = HttpServer::new(|| {
-        App::new()
-            .service(Files::new("/", "../../wasm/dist/index.html").index_file("index.html"))
-    });
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    let socket = std::env::var("SOCK");
+    match std::env::var("SOCK") {
+        Ok(s) => {
+            dbg!(&s);
+            HttpServer::new(|| {
+                App::new()
+                    .service(Files::new("/", "/home/l/le/lead/wasm/dist").index_file("index.html"))
+            })
+            .bind_uds(s)?
+            .workers(1)
+            .run()
+            .await;
+        },
+        Err(e) => {
+            dbg!(e);
+        }
+    };
 
-    if let Ok(socket) = socket {
-        #[cfg(unix)]
-        let listener = UnixListener::bind(socket).unwrap();
-        server = server.bind_uds(socket)?;
-        return server.run().await
-    } else {
-        server = server.bind(("127.0.0.1", 8080))?;
-        return server.run().await
-    }
+    Ok(())
 }
+
+#[cfg(not(unix))]
+fn main() {}
